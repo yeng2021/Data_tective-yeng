cf5d455a098b4822d8ded4ee7f3830e0ef290c59
chocosongsonii <jh041109@gmail.com>
2021-09-29 03:30:19 +0900

    경고문구 사이즈 조절 + 텍스트 블러 추가

--- lib/home.dart
+++ lib/home.dart
@@ -255,8 +255,10 @@ class StickerOption extends StatelessWidget {
             decoration: BoxDecoration(
               border: Border(
                 bottom: BorderSide(
-                  color: selected ? Colors.transparent : const Color(0xff647dee),
-                  width: selected ? 0 : 10,
+                  color: selected ?? false ? const Color(0xff647dee) : Colors.transparent,
+                  width: selected ?? false ? 10 : 0,
+                  // color: selected ? Colors.transparent : const Color(0xff647dee),
+                  // width: selected ? 0 : 10, //TODO 여기에 문제가 있습니다!!
                 )
               )
             ),
@@ -266,7 +268,10 @@ class StickerOption extends StatelessWidget {
                 duration: const Duration(milliseconds: 300),
                 padding: const EdgeInsets.all(8),
                 decoration: BoxDecoration(
-                  color: selected ? Colors.black54 : const Color(0xff7f53ac),
+                  color: selected ?? false
+                      ? const Color(0xff7f53ac)
+                      : Colors.black54,
+                  // color: selected ? Colors.black54 : const Color(0xff7f53ac), //TODO 여기에 문제가 있습니다!!
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Text(
